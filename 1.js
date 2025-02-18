222c6c8d97adde8046aa8826434ab6f5c7a966f1
titi_duong Dương Huy Hưng <titi_duong@arcadyan.com.vn>
2025-02-18 21:04:27 +0700

    up my web

---
+++ 1.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('#experience').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault(); // Ngăn chặn hành vi mặc định của thẻ `<a>`   
+        const targetElement = document.querySelector('#intro');
+
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' }); // Cuộn mượt
+        }
+    });
+});
+document.querySelectorAll('#about').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault(); // Ngăn chặn hành vi mặc định của thẻ `<a>`   
+        const targetElement = document.querySelector('#aboutme');
+
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' }); // Cuộn mượt
+        }
+    });
+}); 
